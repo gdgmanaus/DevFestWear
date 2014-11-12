fbb6be30553eebafb31700adc8f665124bd0a20e
Daniel Nogueira <dnogueira86@gmail.com>
2014-11-12 06:45:14 -0400

    changes made on code lab DesvFest

--- Weareable/src/main/java/com/devfest/gdg/devfestwear/MainActivity.java
+++ Weareable/src/main/java/com/devfest/gdg/devfestwear/MainActivity.java
@@ -1,6 +1,7 @@
 package com.devfest.gdg.devfestwear;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
 import android.view.View;
@@ -29,6 +30,14 @@ public class MainActivity extends Activity {
     public void onFinishActivity(View view) {
         setResult(RESULT_OK);
         finish();
+    }
+
+    /**
+     * Handles the button press to call list activity.
+     */
+    public void onNewActivity(View view) {
+        Intent intent = new Intent(this,ListActivity.class);
+        startActivity(intent);
     }
 
 
